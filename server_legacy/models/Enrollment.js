794b8928e1e05348680cb758c1ad1dfaee7ca911
Amritesh Tiwari <amriteshtiwari2003@gmail.com>
2026-03-05 12:32:26 +0530

    3.0.1.1

--- server_legacy/models/Enrollment.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const enrollmentSchema = new mongoose.Schema({
-    id: { type: String, required: true, unique: true },
-    studentId: { type: String, required: true },
-    courseId: { type: String, required: true }
-}, { timestamps: true });
-
-export const Enrollment = mongoose.model('Enrollment', enrollmentSchema);
